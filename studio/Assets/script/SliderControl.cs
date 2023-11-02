b391cf937365b1220c243177ab426fc63e7199b2
Samuele Ravazzani <samuele.ravazzani@mail.polimi.it>
2023-11-02 18:20:42 +0100

    Added nice button + tried to make texts appear softly
    tried to make texts appear softly: DO NOT WORK

--- studio/Assets/script/SliderControl.cs
+++ studio/Assets/script/SliderControl.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
@@ -11,10 +13,30 @@ public class SliderControl : MonoBehaviour
     public Slider literacy;
     [FormerlySerializedAs("ccskepticism")] public Slider climate_change_skept;
     public string sceneName = "MainMap";
-    
+    public TextMeshProUGUI greetings;
+    public TextMeshProUGUI anxiety_txt;
+    public TextMeshProUGUI literacy_txt;
+    public TextMeshProUGUI climate_change_txt;
+    public float animationduration=2.0f;
+
+    private void Awake()
+    {
+        anxiety.gameObject.SetActive(false);
+        literacy.gameObject.SetActive(false);
+        climate_change_skept.gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        greetings.CrossFadeAlpha(1.0f, animationduration, true);
+        anxiety_txt.CrossFadeAlpha(1.0f, animationduration, true);
+        literacy_txt.CrossFadeAlpha(1.0f, animationduration, true);
+        climate_change_txt.CrossFadeAlpha(1.0f, animationduration, true);
+        anxiety.gameObject.SetActive(true);
+        literacy.gameObject.SetActive(true);
+        climate_change_skept.gameObject.SetActive(true);
+        
         // set sliders to 0
         anxiety.value = 0;
         literacy.value = 0;
