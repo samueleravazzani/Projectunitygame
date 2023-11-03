eb6b095b08087d99fda46918432df7c51d5dae8b
FedeS13 <federicamaria.storti@mail.polimi.it>
2023-11-03 12:59:14 +0100

    Quiz game
    Modifiche al codice di quiz manager
    Ho fatto in modo che anche le risposte associate a ogni domanda fossero generate in maniera random (così come già erano le possibili domande)
    + ho aggiustato il qnA panel
    perchè prima se sceglievo di avere 3 possibili opzioni come answers continuava comunque ad apparirmi il 4 bottone , adesso invece scompare
    Per fare questo il method Set Answers è stato inglobato all'interno di GenerateQuestion

--- studio/Assets/script/QuizManager.cs
+++ studio/Assets/script/QuizManager.cs
@@ -32,6 +32,10 @@ public class QuizManager : MonoBehaviour
     public int score; 
     //keeps track of the player's score.
     
+    private List<int> answerIndices = new List<int>();
+    //answerIndices is a list of integers used to keep track of the indices of answer options for the current question.
+    //It will help with the randomization of answers associated to each question when displayed 
+    
     private void Start()
     {
         totalQuestions = qnA.Count;
@@ -75,8 +79,9 @@ public class QuizManager : MonoBehaviour
 
     }
     
-    //There are methods Correct and wrong for handling correct
+    //These are methods Correct and wrong for handling correct
     //and incorrect answers, respectively.
+    //The correct updates the score
 
     IEnumerator WaitForNext()
     {
@@ -85,26 +90,44 @@ public class QuizManager : MonoBehaviour
     }
     
     //waitForNext coroutine waits for a short period before generating the next question.
-
-    void SetAnswers()
-    {
-        for (int i = 0; i < options.Length; i++)
-        {
-            options[i].GetComponent<Image>().color = options[i].GetComponent<AnswerScript>().startColor;
-            options[i].GetComponent<AnswerScript>().isCorrect = false; 
-            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = qnA[currentQuestion].answers[i];
-            
-            
-            if (qnA[currentQuestion].correctAnswer == i + 1)
-            {
-                options[i].GetComponent<AnswerScript>().isCorrect = true; 
-            }
-        }
-    }
     
-    //SetAnswers method sets the answer choices for the current question
-    //and determines which one is the correct answer
-
+    
+    //THE FOLLOWING METHOD is responsible for setting up a new question for the player to answer during the quiz game
+    //This code ensures that each new question is selected randomly, and the order of the answer options is also randomized
+    // It adjusts the number of active buttons to match the number of answer options for each question.
+    
+    // checks if there are questions left in qnA list (which holds questions and answers).
+    // If there are questions remaining, the method proceeds to select and set up a new question.
+    // If there are no questions left, it logs a message and triggers the game over state.
+    
+    // Randomly selects a question from your qnA list  by generating a random index within the valid range of questions.
+    
+    //Next is to  associate the answers to the question
+    //We have to ensure that answers to the question are disposed randomly
+    //Plus i want that if the answer has 2 possible options are shown only 2, if 3 only 3 etc. (MAX. OF 4 POSSIBLE OPTIONS WAS HYPHOTETIZED BY DEFAULT WHEN BUILDING THE GAME )
+    
+    //Clear the answerIndices list and populate it with indices that represent the available answer options for the current question.
+    //The for loop iterates through the answer options of the current question
+    //For each answer option, it adds the index i to the answerIndices list.
+    //This means that answerIndices will contain the indices of answer options in their original order.
+    
+    //This code shuffles the answerIndices list to randomize the order of answer options. It uses the Fisher-Yates shuffle algorithm to achieve this. 
+    //    The for loop iterates backward through the list of answerIndices, starting from the last index
+    //    (the one before the last element) and moving towards the beginning of the list.
+    //    Inside the loop, a random index j is generated using Random.Range(0, i + 1).
+    //    This index j represents a position in the list of answer indices.
+    //    The code then performs a swap operation: It temporarily stores the value at index i in a variable temp,
+    //    then replaces the value at index i with the value at index j,
+    //    and finally replaces the value at index j with the temporary value temp.
+    
+    //The code activates the buttons corresponding to the number of answer options for the current question.
+    //It sets the button text and determines which answer is correct 
+    //Any extra buttons that are not needed are deactivated.
+    
+    //If there are no questions left (i.e., qnA.Count becomes zero), the method logs a message ("Out of Questions")
+    //and triggers the game over state.
+    
+    
     void GenerateQuestion()
     {
         if (qnA.Count > 0)
@@ -112,7 +135,44 @@ public class QuizManager : MonoBehaviour
             currentQuestion = Random.Range(0, qnA.Count);
 
             questionTxt.text = qnA[currentQuestion].question;
-            SetAnswers();
+
+            // Populate answerIndices with random indices
+            answerIndices.Clear(); 
+            for (int i = 0; i < qnA[currentQuestion].answers.Length; i++)
+            {
+                answerIndices.Add(i);
+            }
+
+            
+            // Shuffle answerIndices to randomize the order
+            for (int i = answerIndices.Count - 1; i > 0; i--)
+            {
+                int j = Random.Range(0, i + 1);
+                int temp = answerIndices[i];
+                answerIndices[i] = answerIndices[j];
+                answerIndices[j] = temp;
+            }
+            
+            // Activate buttons based on the number of answer options
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i < answerIndices.Count)
+                {
+                    options[i].SetActive(true); //// Activate a button
+                    options[i].GetComponent<Image>().color = options[i].GetComponent<AnswerScript>().startColor;
+                    options[i].GetComponent<AnswerScript>().isCorrect = false;
+                    options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = qnA[currentQuestion].answers[answerIndices[i]];
+
+                    if (qnA[currentQuestion].correctAnswer == answerIndices[i] + 1)
+                    {
+                        options[i].GetComponent<AnswerScript>().isCorrect = true;
+                    }
+                }
+                else
+                {
+                    options[i].SetActive(false); //// Deactivate any extra buttons that are not needed.
+                }
+            }
         }
         else
         {
@@ -120,7 +180,5 @@ public class QuizManager : MonoBehaviour
             GameOver();
         }
     }
-    //GenerateQuestion method selects a random question from the list and updates the question text and answer choices.
-    //If no questions are left, it calls GameOver.
-    
+ 
 }
