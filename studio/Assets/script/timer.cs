ac76ad2fe11561ccbeefe8d119601be827d24965
Stefano Rossi <stefano19.rossi@mail.polimi.it>
2023-11-04 18:32:59 +0100

    breathing game
    sistemazione codice timer con scritte senza azzeramento

--- studio/Assets/script/timer.cs
+++ studio/Assets/script/timer.cs
@@ -6,18 +6,35 @@ using TMPro;
 public class timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI TimerText;
+    [SerializeField] private TextMeshProUGUI PhaseText;
     private float elapsedTime;
+    private int seconds;
+    private int phase;
 
     // Update is called once per frame
     void Update()
     {
         if (ResizeObject.GetInstance().begin)
         {
-                elapsedTime += Time.deltaTime;
-                int seconds = Mathf.FloorToInt(elapsedTime % 60);
-                TimerText.text = string.Format("{0:00}", seconds);
-            }
-            
+            elapsedTime += Time.deltaTime;
+            seconds = Mathf.FloorToInt(elapsedTime % 60);
+            TimerText.text = string.Format("{0:00}", seconds);
         }
+        
+        if (seconds>=1 && seconds <= 3)
+        {
+            PhaseText.text = "inhale";
+        }
+
+        if (seconds>3 && seconds <= 7)
+        {
+            PhaseText.text = "hold";
+        }
+        if (seconds>7 && seconds <= 14)
+        {
+            PhaseText.text = "exhale";
+        }
+        
+        
     }
 }
