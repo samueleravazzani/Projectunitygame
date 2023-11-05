a23d0977009c7854a55c3dbfa854921eead108e0
Samuele Ravazzani <samuele.ravazzani@mail.polimi.it>
2023-11-05 19:41:49 +0100

    Creato card template delle medicine
    Creato card template delle medicine e iniziato a implementarlo tramite ScriptableObjects

---
+++ studio/Assets/CardDisplay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Medicine_Card card;
+    public TextMeshProUGUI drug_name;
+    public TextMeshProUGUI drug_class;
+    public TextMeshProUGUI drug_indication;
+    public TextMeshProUGUI drug_warnings;
+    public Sprite drug_image;
+    public Image background;
+    
+
+}
