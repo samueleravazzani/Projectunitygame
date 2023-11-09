0864e24e7a2d8750824e3f3ac849de672cdac344
Samuele Ravazzani <samuele.ravazzani@mail.polimi.it>
2023-11-09 22:36:21 +0100

    Environment Effects
    - Fire -> colore camera
    - inizio della saturazione

--- studio/Assets/EnvironmentControl.cs
+++ studio/Assets/EnvironmentControl.cs
@@ -16,30 +16,64 @@ public class EnvironmentControl : MonoBehaviour
     
     private float[] xlim = new float[] {-20f, 28f}, ylim = new float[] {-15f, 20.5f};
 
-    public int[] problems = new int[] { 1, 2, 3, 4, 5, 6 };
+    private int[] problems = new int[] {1, 2, 3, 4, 5, 6};
     // 1 = fire, 2 = plastic, 3 = water, 4 = pollution, 5 = air, 6 = rain;
     private int problem_now = 1;
-    private int N_tospawn = 200;
     
+    public static int N_tospawn = 200; // questo cambia durante il gioco /!\
+    public static float level_anxiety = 10f, calibration_anxiety = -7f;
+    public static bool update_camera_bool = true;
+    
+    // vettore di colori
+    private Color[] fireColors = {new Color(0.91509f, 0.3151032f, 0.3151032f), new Color (0.9433962f, 0.4850481f,  0.4850481f), new Color (1.0f,1.0f,1.0f)};
+    public static int color_index=0;
+
     // Start is called before the first frame update
     void Start()
     {
         
-        switch (problem_now)
-        {
-            case 1:
-                Spawn(fire, N_tospawn);
-                postProcessingVolume.profile.GetSetting<ColorGrading>().colorFilter.value = new Color(1.0f, 0.0f, 0.0f);
-                break;
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (update_camera_bool)
+        {
+            UpdateEnvironment();
+        }
     }
 
+    private void UpdateEnvironment()
+    {
+        switch (problem_now)
+        {
+            case 1:
+                Spawn(fire, N_tospawn);
+                CameraEnvironment(fireColors[color_index]);
+                CameraAnxiety(level_anxiety * calibration_anxiety);
+                break;
+            case 2:
+                Spawn(plastic, N_tospawn);
+                CameraEnvironment(fireColors[color_index]);
+                CameraAnxiety(level_anxiety * calibration_anxiety);
+                break;
+        }
+        update_camera_bool = false;
+    }
+
+    private void CameraAnxiety(float level)
+    {
+        postProcessingVolume.profile.GetSetting<ColorGrading>().saturation.value = level;
+    }
+    
+    
+    private void CameraEnvironment(Color color)
+    {
+        postProcessingVolume.profile.GetSetting<ColorGrading>().colorFilter.value = color;
+    }
+    
+    
     private void Spawn(Sprite[] obj_s, int N)
     {
         for (int i = 0; i < N; i++)
