7d8bc39a708f218d94352736a4ac75461ac1cc47
FedeS13 <federicamaria.storti@mail.polimi.it>
2023-11-26 19:37:16 +0100

    Bozza non buona slider

---
+++ studio/Assets/Playerhealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Playerhealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Heallthbarscript healthBar;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
