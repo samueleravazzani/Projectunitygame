9c508a61c9414dc2b4881c23f5c711382e83b536
Samuele Ravazzani <samuele.ravazzani@mail.polimi.it>
2023-12-05 16:07:18 +0100

    Tasks_picked messo public

--- studio/Assets/script/GameManager.cs
+++ studio/Assets/script/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
     public float literacy_inverted = 5; // settato in SliderControl
     public float climate_change_skept = 5; // settato in SliderControl
     public float sum_parameters = 15; // settato in SliderControl
-    public static int[] tasks_picked = new int[3]; // vettore di 3 int: [0] per anxiety, [1] per literacy, [2] per climate change
+    public int[] tasks_picked = new int[3]; // vettore di 3 int: [0] per anxiety, [1] per literacy, [2] per climate change
     // in ciascuno di questi 3 elementi pesco un numero casuale che definisce la task
     // ANXIETY: 0 fatta, 1 breathing, 2 music, 3 word puzzle
     // LITERACY: 0 fatta, 1 postions, 2 sources
