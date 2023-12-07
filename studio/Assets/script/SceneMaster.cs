5741cd5af402bf1a4ad28c3b94b972185a115eb9
Stefano Rossi <stefano19.rossi@mail.polimi.it>
2023-12-07 13:08:25 +0100

    transizione scene
    implementazione della transizione tra una scena ad un altra, aggiungere il load transition scene nella scena in cui si vuole la transizione

--- studio/Assets/script/SceneMaster.cs
+++ studio/Assets/script/SceneMaster.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneMaster : MonoBehaviour
 {
     public GameObject player;
     public GameObject Bever;
     public static SceneMaster instance;
+    public float transitionTime = 1f;
+    public Animator transition;
 
     void Awake()
     {
@@ -28,8 +31,10 @@ public class SceneMaster : MonoBehaviour
 
     public void ChangeSchene(string scenetoload, bool playeractive, Vector3 playerposition)
     {
-        SceneManager.LoadScene(scenetoload); // carico la scena
-        player.gameObject.SetActive(playeractive); // attivo/disattivo il player
+        transition = GameObject.Find("SceneLoaderTransition/Crossfade").GetComponent<Animator>();
+        //SceneManager.LoadScene(scenetoload); // carico la scena
+        StartCoroutine(LoadTransitionScene(scenetoload,playeractive,playerposition));
+       /* player.gameObject.SetActive(playeractive); // attivo/disattivo il player
         player.GetComponent<playerMovement>().enabled = playeractive; // attivo/disattivo il movimento del player
         Bever.gameObject.SetActive(playeractive);
         
@@ -38,6 +43,39 @@ public class SceneMaster : MonoBehaviour
             player.transform.position = playerposition;
             // non funziona player.GetComponent<playerMovement>().worldBorders = GameObject.Find("WorldBorders").GetComponent<PolygonCollider2D>();
             Bever.transform.position = playerposition;
+        }
+        
+        // se la scena da caricare è la MainMap -> salvo
+        if (scenetoload == "MainMap")
+        {
+            GameManager.instance.Save();
+        }
+        */
+        
+    }
+
+    IEnumerator LoadTransitionScene (string scenetoload,bool playeractive,Vector3 playerposition)
+    {
+        player.GetComponent<playerMovement>().enabled = false; 
+        player.GetComponent<Animator>().enabled = false; 
+        player.GetComponent<SpriteRenderer>().enabled = false; 
+        Bever.GetComponent<SpriteRenderer>().enabled = false;
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scenetoload);
+        player.gameObject.SetActive(playeractive); // attivo/disattivo il player
+        player.GetComponent<playerMovement>().enabled = playeractive;
+        player.GetComponent<Animator>().enabled = playeractive; // attivo/disattivo il movimento del player
+        
+        Bever.gameObject.SetActive(playeractive);
+        
+        if (playeractive) // se serve il player -> setto la posizione
+        {
+            player.transform.position = playerposition;
+            // non funziona player.GetComponent<playerMovement>().worldBorders = GameObject.Find("WorldBorders").GetComponent<PolygonCollider2D>();
+            Bever.transform.position = playerposition;
+            player.GetComponent<SpriteRenderer>().enabled = playeractive; 
+            Bever.GetComponent<SpriteRenderer>().enabled = playeractive;
         }
         
         // se la scena da caricare è la MainMap -> salvo
