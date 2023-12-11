b49296e4fde8851c05fc578872f640123f4647c9
Roberto di Giacomo <roberto1.digiacomo@mail.polimi.it>
2023-12-11 19:02:53 +0100

    ClimateChnage Fire Scene, Intro + Button Logic

---
+++ studio/Assets/script/Climate_Change_Param.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Climate_Change_Param : MonoBehaviour
+{
+    public void Parametrization()
+    {
+        if (GameManager.instance.climate_change_skept >= 1 && GameManager.instance.climate_change_skept <= 3)
+        {
+            SceneManager.LoadScene("Climate_Change_Fuoco");
+        }
+        else if (GameManager.instance.climate_change_skept >= 4 && GameManager.instance.climate_change_skept <= 6)
+        {
+            SceneManager.LoadScene("Climate_Change_Fuoco1");
+        }
+        else if (GameManager.instance.climate_change_skept >= 7 && GameManager.instance.climate_change_skept <= 9)
+        {
+            SceneManager.LoadScene("Climate_Change_Fuoco2");
+        }
+ 
+    }
+}
