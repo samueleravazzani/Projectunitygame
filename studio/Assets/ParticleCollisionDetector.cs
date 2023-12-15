dabe00586defc8f99b4b76297080bf92f538ad05
Roberto di Giacomo <roberto1.digiacomo@mail.polimi.it>
2023-12-15 11:25:33 +0100

    removing ZEN word

--- studio/Assets/ParticleCollisionDetector.cs
+++ studio/Assets/ParticleCollisionDetector.cs
@@ -1,16 +1,18 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ParticleCollisionDetector : MonoBehaviour
 {
     private ParticleSystem particleSystem;
-    private bool state = false;
+    private bool state;
     public MovesCounter movesCounter;
     
 
     private void Start()
     {
         particleSystem = GetComponent<ParticleSystem>();
+        state = false;
     }
 
     private void OnParticleCollision(GameObject other)
@@ -38,13 +40,15 @@ public class ParticleCollisionDetector : MonoBehaviour
                 fireSystem.SetActive(false);
                 wall31.SetActive(false);
                 particleSystem.gameObject.SetActive(false);
+                SceneManager.LoadScene("Climate_Change_Fuoco"); 
+
             }
         }
-        if (!other.CompareTag("Disattiva"))
+        if (!other.CompareTag("Disattiva") && state==false)
         {
-            Debug.Log(movesCounter.remainingMoves);
-            if (movesCounter.remainingMoves == 0 && state==false)
+            if (movesCounter.remainingMoves == 0)
             {
+                Debug.Log(state);
                 movesCounter.Invoke("ShowPopup",1f);
             }
 
